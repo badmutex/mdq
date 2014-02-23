e81975579f0f84fb92dd2dbeabd0567b93e751f4
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-02-23 16:11:13 -0500

    save

---
+++ mdq/md/gmx.py
@@ -0,0 +1,72 @@
+
+import mdprep
+import os
+import shlex
+import subprocess
+import textwrap
+
+script = textwrap.dedent("""\
+#!/usr/bin/env bash
+# exit if any command fails
+set -o errexit 
+
+# disable gromacs automatic backups
+export GMX_MAXBACKUP=-1
+
+continue from previous positions, velocities, and time
+guamps_set -f topol.tpr -s positions  -i positions.gps
+guamps_set -f topol.tpr -s velocities -i velocities.gps
+guamps_set -f topol.tpr -s time       -i time.gps
+
+# run with given number of processors
+mdrun -nt $(cat processors.gps)
+
+# retrieve the positions, velocities, and time
+guamps_get -f traj.trr -s positions  -o x.gps
+guamps_set -f traj.trr -s velocities -o v.gps
+guamps_get -f traj.trr -s time       -o t.gps
+""")
+
+
+executables = [
+      'guamps_get'
+    , 'guamps_set'
+    , 'mdrun'
+]
+
+def get_nsteps(path):
+    suffix = os.path.splitext(path)[-1]
+    suffix = suffix.lower()
+    if suffix == '.tpr':
+        cmd = 'guamps_get -f %(tpr)s -s nsteps' % dict(tpr=path)
+    elif suffix == '.mdp':
+        cmd = "egrep '^ *nsteps *=' %(mdp)s | awk '{print $3}'" % dict(mdp=path)
+    else:
+        raise Exception, '%s has unknown filetype %s' % (path, suffix)
+
+    out = subprocess.check_output(cmd, shell=True).strip()
+    nsteps = int(out)
+    return nsteps
+
+
+class GMX(object):
+
+    def __init__(self, workarea='.', generation=1,
+                 gro='conf.gro',
+                 top='topol.top',
+                 itp='posre.itp',
+                 mdp='grompp.mdp'):
+
+        self._workarea = workarea
+        self._gens     = generations
+        self._gro      = os.path.join(workarea, gro)
+        self._top      = os.path.join(workarea, top)
+        self._itp      = os.path.join(workarea, itp)
+        self._mdp      = os.path.join(workarea, mdp)
+        self._gensteps = None
+
+    def _set_seed(self, seed):
+        mdp = mdprep.load(self._mdp)
+        mdp.seed(seed)
+        mdp.save(self._mdp)
+
